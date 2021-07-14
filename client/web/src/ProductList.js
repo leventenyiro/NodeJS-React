9b8cd0b37273b95f54ff8d12c4a18d58360dd2a8
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-14 21:01:49 +0200

    add bootstrap

--- client/web/src/ProductList.js
+++ client/web/src/ProductList.js
@@ -1,4 +1,5 @@
 import { Link } from "react-router-dom";
+import { Container, Row, Col } from 'react-bootstrap'
 
 const ProductList = ({ products, title }) => {
     return (
@@ -6,11 +7,18 @@ const ProductList = ({ products, title }) => {
             <h2>{ title }</h2>
             { products.map((product) =>
                 <div className="product-preview" key={ product.id }>
-                    <Link to={`/product/${ product.id }`}>
+                    {/*<Link to={`/product/${ product.id }`}>
                         <h2>{ product.name }</h2>
                         <p>$ { product.price }</p>
                         <p>{ product.active === 1 ? product.active = "Available" : product.active = "Not available" }</p>
-                    </Link>
+            </Link>*/}
+                    <Row>
+                        <Col><h2>{ product.name }</h2></Col>
+                        <Col><p>$ { product.price }</p></Col>
+                        <Col><p>{ product.active === 1 ? product.active = "Available" : product.active = "Not available" }</p></Col>
+                    </Row>
+                    
+                    
                 </div>
             )}
         </div>
