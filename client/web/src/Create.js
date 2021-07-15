73298386088656c80e514415db5188ddf560107b
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-15 22:00:48 +0200

    react changes in create

--- client/web/src/Create.js
+++ client/web/src/Create.js
@@ -1,5 +1,7 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
+import { Container } from 'react-bootstrap'
+import Form from 'react-bootstrap/Form'
 
 const Create = () => {
     const [name, setName] = useState();
@@ -26,36 +28,49 @@ const Create = () => {
 
     return (
         <div className="create">
+            <Container>
             <h2>Add new product</h2>
-            <form onSubmit={handleSubmit}>
-                <label>Name:</label>
-                <input 
-                    type="text"
-                    required
-                    value={name}
-                    onChange={(e) => setName(e.target.value)}
-                />
+                <Form onSubmit={handleSubmit}>
+                    <Form.Group>
+                        <Form.Label>Name:</Form.Label>
+                        <Form.Control 
+                            type="text"
+                            required
+                            value={name}
+                            onChange={(e) => setName(e.target.value)}
+                        />
+                    </Form.Group>
+                    
+                    <Form.Group>
+                        <Form.Label>Price:</Form.Label>
+                        <Form.Control
+                            type="number"
+                            min="0"
+                            required
+                            value={price}
+                            onChange={(e) => setPrice(e.target.value)}        
+                        />
+                    </Form.Group>
+                    
+                    <Form.Group>
+                        <Form.Label>Active:</Form.Label>
+                        <Form.Control
+                            as="select"
+                            value={active}
+                            onChange={(e) => setActive(e.target.value)}
+                        >
+                            <option value="1">Active</option>
+                            <option value="0">Inactive</option>
+                        </Form.Control>
+                    </Form.Group>
 
-                <label>Price:</label>
-                <input
-                    type="number"
-                    min="0"
-                    required
-                    value={price}
-                    onChange={(e) => setPrice(e.target.value)}        
-                />
+                    { !isPending && <button variant="primary" type="submit">Add product</button> }
+                    { isPending && <button variant="primary" type="submit" disabled>Adding product...</button> }
+                    
+                    
+                </Form>
+            </Container>
 
-                <label>Active:</label>
-                <select
-                    value={active}
-                    onChange={(e) => setActive(e.target.value)}
-                >
-                    <option value="1">Active</option>
-                    <option value="0">Inactive</option>
-                </select>
-                { !isPending && <button>Add product</button> }
-                { isPending && <button disabled>Adding product...</button> }
-            </form>
         </div>
     )
 }
