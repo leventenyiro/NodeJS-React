67e448ad669c6742308f388e8de87e600ecaf7d7
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-17 21:55:37 +0200

    add delete to web

--- client/web/src/ProductDetails.js
+++ client/web/src/ProductDetails.js
@@ -1,12 +1,22 @@
-import { useParams } from "react-router-dom";
+import { useHistory, useParams } from "react-router-dom";
 import useFetch from "./useFetch";
 
 const ProductDetails = () => {
     const { id } = useParams()
     const url = "http://localhost:8080/product/" + id
-    console.log(url)
     const { data: product, error, isPending } = useFetch(url);
+    const history = useHistory()
     
+    const handleClick = () => {
+        const url = "http://localhost:8080/product/" + product.id
+        console.log(url)
+        fetch(url, {
+            method: "DELETE"
+        }).then(() => {
+            history.push("/");
+        })
+    }
+
     return (
         <div className="product-details">
             { isPending && <div>Loading</div> }
@@ -16,6 +26,7 @@ const ProductDetails = () => {
                     <h2>{ product.name }</h2>
                     <p>$ { product.price }</p>
                     <p>{ product.active === 1 ? product.active = "Available" : product.active = "Not available" }</p>
+                    <button onClick={handleClick}>Delete</button>
                 </article>
             )}
         </div>
--- client/web/src/ProductList.js
+++ client/web/src/ProductList.js
@@ -1,7 +1,17 @@
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 //import { Container, Row, Col } from 'react-bootstrap'
 
 const ProductList = ({ products, title }) => {
+    const handleDelete = (id) => {
+        const url = "http://localhost:8080/product/" + id
+        fetch(url, {
+            method: "DELETE"
+        }).then(() => {
+            window.location.reload() // EHELYETT KÉNE VALAMI
+        })
+    }
+    
     return (
         <div className="product-list">
             <h2>{ title }</h2>
@@ -12,6 +22,8 @@ const ProductList = ({ products, title }) => {
                         <p>$ { product.price }</p>
                         <p>{ product.active === 1 ? product.active = "Available" : product.active = "Not available" }</p>
                         <Link className="btn btn-primary" to={`/product/${ product.id }`}>Update</Link>
+
+                        <button className="btn btn-danger" onClick={() => handleDelete(product.id)}>Delete</button>
                     {/*<Row>
                         <Col><h2>{ product.name }</h2></Col>
                         <Col><p>$ { product.price }</p></Col>
@@ -22,7 +34,7 @@ const ProductList = ({ products, title }) => {
                 </div>
             )}
         </div>
-    );
+    )
 }
  
 export default ProductList;
