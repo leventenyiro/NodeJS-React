73b24debbf967e80d95f84b102184b5be92c5834
Levente Nyírő <nyiro.levente@gmail.com>
2021-07-21 08:30:04 +0200

    add web fetch

--- client/web/src/Home.js
+++ client/web/src/Home.js
@@ -1,17 +1,37 @@
-import ProductList from './ProductList';
-import useFetch from './useFetch';
+import { useEffect, useState } from "react"
+//import ProductList from "./ProductList"
+import useFetch from "./useFetch"
 
 const Home = () => {
-    const {data: products, isPending, error} = useFetch("http://localhost:8080/product")
-    //console.log(products)
+    const { data: prod, isPending, error } = useFetch("http://localhost:8080/product")
+    const [products, setProducts] = useState(null)
+    if (prod && products == null)
+        setProducts(prod)
+
+    const handleDelete = (id) => {
+        fetch("http://localhost:8080/product/" + id, {
+            method: "DELETE"
+        }).then(() => {
+            const newProducts = products.filter(product => product.id !== id)
+            setProducts(newProducts)
+        })
+    }
+
+    useEffect(() => {}, [products])
     
     return (
         <div className="home">
             {error && <div>{ error }</div>}
             {isPending && <div>Loading...</div>}
-            {products && <ProductList products={products} title="All products" />}
+            {/*products && <ProductList products={products} handleDelete={handleDelete}/>*/}
+            {products && products.map(product => (
+                <div key={product.id}>
+                    <p>{product.id} - {product.name} - {product.price}</p>
+                    {<button onClick={() => handleDelete(product.id)}>Delete</button>}
+                </div>
+            ))}
         </div>
-    );
+    )
 }
  
 export default Home;
