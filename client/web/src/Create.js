e46a031be726a8e2447167d4057e9aba67a20f2d
Levente Nyírő <nyiro.levente@gmail.com>
2021-10-27 19:04:26 +0200

    create debug

--- client/web/src/Create.js
+++ client/web/src/Create.js
@@ -1,13 +1,11 @@
 import { useState } from "react";
 import { useHistory } from "react-router";
-import { Container } from 'react-bootstrap'
-import Form from 'react-bootstrap/Form'
 
 const Create = () => {
-    const [name, setName] = useState();
-    const [price, setPrice] = useState();
-    const [active, setActive] = useState();
-    const [isPending, setIsPending] = useState(false);
+    const [name, setName] = useState()
+    const [price, setPrice] = useState()
+    const [active, setActive] = useState()
+    const [isPending, setIsPending] = useState()
     const history = useHistory();
 
     const handleSubmit = (e) => {
@@ -27,50 +25,24 @@ const Create = () => {
     }
 
     return (
-        <div className="create">
-            <Container>
-            <h2>Add new product</h2>
-                <Form onSubmit={handleSubmit}>
-                    <Form.Group>
-                        <Form.Label>Name:</Form.Label>
-                        <Form.Control 
-                            type="text"
-                            required
-                            value={name}
-                            onChange={(e) => setName(e.target.value)}
-                        />
-                    </Form.Group>
-                    
-                    <Form.Group>
-                        <Form.Label>Price:</Form.Label>
-                        <Form.Control
-                            type="number"
-                            min="0"
-                            required
-                            value={price}
-                            onChange={(e) => setPrice(e.target.value)}        
-                        />
-                    </Form.Group>
-                    
-                    <Form.Group>
-                        <Form.Label>Active:</Form.Label>
-                        <Form.Control
-                            as="select"
-                            value={active}
-                            onChange={(e) => setActive(e.target.value)}
-                        >
-                            <option value="1">Active</option>
-                            <option value="0">Inactive</option>
-                        </Form.Control>
-                    </Form.Group>
+        <div>
+            <button className="btn btn-primary" onClick={history.goBack}>Back</button>
+            <div id="product-details">
+                <form>
+                    <label htmlFor="inputName">Name</label><br />
+                    <input type="text" id="inputName" value={name} onChange={(e) => setName(e.target.value)} /><br />
 
-                    { !isPending && <button variant="primary" type="submit">Add product</button> }
-                    { isPending && <button variant="primary" type="submit" disabled>Adding product...</button> }
-                    
-                    
-                </Form>
-            </Container>
+                    <label htmlFor="inputPrice">Price</label><br />
+                    <input type="number" id="inputPrice" value={price} onChange={(e) => setPrice(e.target.value)} /><br />
 
+                    <label htmlFor="inputActive">Activity</label>
+                    <input type="checkbox" id="inputActive" onChange={(e) => setActive(e.target.checked)}/>
+                    <br />
+
+                    { !isPending && <button className="btn btn-primary" type="submit" onClick={handleSubmit}>Add product</button> }
+                    { isPending && <button className="btn btn-primary" type="submit" disabled>Adding product...</button> }
+                </form>
+            </div>
         </div>
     )
 }
