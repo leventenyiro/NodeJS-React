a15c2ee9ec8d66a8a2341ca12d8ac136e5aa74bf
Levente Nyírő <nyiro.levente@gmail.com>
2021-11-14 12:13:42 +0100

    repair model

--- app/models/product.model.js
+++ app/models/product.model.js
@@ -9,8 +9,7 @@ const Product = function(product) {
 Product.getAll = result => {
     conn.query("SELECT * FROM product", (err, res) => {
         if (err) {
-            result(err, null)
-            return
+            return result(err, null)
         }
         
         result(null, res)
@@ -20,67 +19,46 @@ Product.getAll = result => {
 Product.getById = (id, result) => {
     var sql = `SELECT * FROM product WHERE id = "${id}"`
     conn.query(sql, (err, res) => {
-        if (err) {
-            result(err, null)
-            return
-        }
-        if (res.length) {
-            result(null, res[0])
-            return
-        }
+        if (err)
+            return result(err, null)
+        if (res.length)
+            return result(null, res[0])
         result({ kind: "not_found" }, null)
     })
 }
 
 Product.create = (product, result) => {
     conn.query("INSERT INTO product SET ?", product, (err, res) => {
-        if (err) {
-            result(err, null)
-            return
-        }
+        if (err)
+            return result(err, null)
         result(null)
     })
 }
 
 Product.update = (id, product, result) => {
     conn.query("UPDATE product SET name = ?, price = ?, active = ? WHERE id = ?", [product.name, product.price, product.active, id], (err, res) => {
-        if (err) {
-            result(null, err)
-            return
-        }
-
-        if (res.affectedRows == 0) {
-            result({ kind: "not_found" }, null)
-            return
-        }
-
+        if (err)
+            return result(null, err) 
+        if (res.affectedRows == 0)
+            return result({ kind: "not_found" }, null)
         result(null)
     })
 }
 
 Product.delete = (id, result) => {
     conn.query("DELETE FROM product WHERE id = ?", id, (err, res) => {
-        if (err) {
-            result(err, null)
-            return
-        }
-
-        if (res.affectedRows == 0) {
-            result({ kind: "not_found" }, null)
-            return
-        }
-
+        if (err)
+            return result(err, null)
+        if (res.affectedRows == 0)
+            return result({ kind: "not_found" }, null)
         result(null)
     })
 }
 
 Product.deleteAll = result => {
     conn.query("DELETE FROM product", (err, res) => {
-        if (err) {
-            result(err, null)
-            return
-        }
-
+        if (err)
+            return result(err, null)
         result(null)
     })
 }
